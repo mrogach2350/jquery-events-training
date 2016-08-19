80ee843b476b727760bdda8e1b6d5dfa44aff4d8
mrogach2350 <m.rogach@gmail.com>
2016-08-18 21:16:45 -0700

    Fixing click problem

--- solutions/click/base.js
+++ solutions/click/base.js
@@ -3,7 +3,7 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("span").on("click", function handleClick(event){
-    var itemText = "You Clicked: " + $(this).text() + " at " + Date.now();
+    var itemText = "You Clicked: " + (this).text() + " at " + Date.now();
     $("ul").append("<li>" + itemText + "</li>")
   })
 
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(function (){
+
+  $('span').on('click', function handleClick(event){
+    var clicked = "You Clicked " + (this).text() + " at " + Date.now();
+    $("ul").append('<li>' + clicked + "</li>")
+  })
 
 })
